d4564388daf65fb0ebb9b5b8bab559b5ded16933
ThoNohT <e.c.p.bataille@gmail.com>
2021-05-04 18:32:19 +0200

    Delegate running task to dedicated function and change tasks to Map

--- app/Main.hs
+++ app/Main.hs
@@ -1,17 +1,11 @@
 module Main where
 
 import Environment (ensureBasePathExists, getBasePath)
-import Task (Task (..), pickTask)
+import Task (Task (..), pickTask, runTask)
 import Text.Printf (printf)
 
 main :: IO ()
 main = do
     basePath <- getBasePath
-
     ensureBasePathExists basePath
-    task <- pickTask basePath
-
-    case task of
-        Quit -> pure ()
-        NewProfile -> putStrLn "Creating a new profile."
-        StartProfile name -> putStrLn $ printf "Starting profile %s." name
+    pickTask basePath >>= runTask
--- app/Task.hs
+++ app/Task.hs
@@ -1,16 +1,18 @@
-module Task (Task (..), pickTask) where
+module Task (Task (..), pickTask, runTask) where
 
 import Control.Arrow ((>>>))
 import Control.Monad (forM_)
 import Data.Function ((&))
 import Data.Functor ((<&>))
-import Data.List (find)
+import Data.Map.Strict as Map (Map, fromList, lookup)
 import Data.Text as T (Text, isPrefixOf)
 import Environment (baseName)
 import Filesystem (listDirectory)
 import qualified Filesystem.Path.CurrentOS as FP (FilePath)
 import Text.Printf (printf)
 import Text.Read (readMaybe)
+import Data.Map (toList)
+import GHC.Exts (sortWith)
 
 data Task
     = NewProfile
@@ -18,23 +20,33 @@ data Task
     | StartProfile Text
 
 -- Parses a string into a task. If a task can be parsed, it is returned, otherwise Nothing is returned.
-parseTask :: [(Int, FP.FilePath)] -> String -> Maybe Task
+parseTask :: Map Int FP.FilePath  -> String -> Maybe Task
 parseTask subDirs "q" = Just Quit
 parseTask subDirs "new" = Just NewProfile
-parseTask subDirs other = number >>= (\n -> find (fst >>> (== n)) subDirs) <&> (snd >>> baseName >>> StartProfile)
+parseTask subDirs other = number >>= (`Map.lookup` subDirs) <&> (baseName >>> StartProfile)
   where
     number = readMaybe other :: Maybe Int
 
 -- Picks a task and returns it.
 pickTask :: FP.FilePath -> IO Task
 pickTask basePath = do
-    subDirs <- listDirectory basePath <&> filter (not . T.isPrefixOf "." . baseName) <&> zip [1 ..]
-    subDirs & forM_ $ \(idx, sd) -> putStrLn $ printf "%d: %s" idx (baseName sd)
+    subDirs <- listDirectory basePath <&> filter (not . T.isPrefixOf "." . baseName) <&> zip [1 ..] <&> fromList
+    subDirs & toList & sortWith fst & forM_ $ \(idx, sd) -> putStrLn $ printf "%d: %s" idx (baseName sd)
     readTaskFromInput subDirs
 
 -- Reads a task from the console, if a task could not be parsed from the input, the task is asked again.
-readTaskFromInput :: [(Int, FP.FilePath)] -> IO Task
+readTaskFromInput :: Map Int FP.FilePath  -> IO Task
 readTaskFromInput subDirs = do
     putStrLn "Please choose a profile ('new' to create a new one, 'q' to quit)"
     task <- parseTask subDirs <$> getLine
     maybe (readTaskFromInput subDirs) pure task
+
+-- Runs a task.
+runTask :: Task -> IO ()
+runTask Quit = pure ()
+
+runTask NewProfile = do
+    pure ()
+
+runTask (StartProfile profile) =
+    pure ()
