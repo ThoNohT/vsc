0851c0a5c9025ae47a88c50dcee0b0f72fc728ed
ThoNohT <e.c.p.bataille@gmail.com>
2021-05-04 08:52:16 +0200

    Small changes and apply formatter

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # vsc
 Visual Studio Code Profile management tool.
+
+
+## Formatting provider
+fourmolu
--- app/Main.hs
+++ app/Main.hs
@@ -1,17 +1,16 @@
 module Main where
 
-import Control.Arrow ( (>>>) )
-import Data.Functor ( (<&>) )
-import Data.Function ( (&) )
+import Control.Arrow ((>>>))
+import Control.Monad (forM_)
+import Data.Function ((&))
+import Data.Functor ((<&>))
+import Data.List (find)
 import Data.Text (Text)
 import qualified Data.Text as T
-import Filesystem ( createTree, getHomeDirectory, isDirectory, listDirectory )
-import Filesystem.Path.CurrentOS as FP ( append, encodeString, fromText, FilePath )
-import Control.Monad (forM_)
+import Filesystem (createTree, getHomeDirectory, isDirectory, listDirectory)
+import Filesystem.Path.CurrentOS as FP (FilePath, append, encodeString, fromText)
 import Text.Printf (printf)
 import Text.Read (readMaybe)
-import Data.List (find)
-
 
 data Task
     = NewProfile
@@ -36,24 +35,24 @@ baseName = encodeString >>> T.pack >>> T.split (\x -> x == '/' || x == '\\') >>>
 -- Picks a task and returns it.
 pickTask :: FP.FilePath -> IO Task
 pickTask basePath = do
-    subDirs <- listDirectory basePath <&> filter (\x -> baseName x & T.isPrefixOf "." & not) <&> zip [1..]
-    subDirs & forM_ $ \(idx, sd) -> do
-        putStrLn $ printf "%d: %s" idx (baseName sd)
+    subDirs <- listDirectory basePath <&> filter (not . T.isPrefixOf "." . baseName) <&> zip [1 ..]
+    subDirs & forM_ $ \(idx, sd) -> putStrLn $ printf "%d: %s" idx (baseName sd)
     readTaskFromInput subDirs
 
 -- Reads a task from the console, if a task could not be parsed from the input, the task is asked again.
-readTaskFromInput :: [ (Int, FP.FilePath) ] ->  IO Task
+readTaskFromInput :: [(Int, FP.FilePath)] -> IO Task
 readTaskFromInput subDirs = do
     putStrLn "Please choose a profile ('new' to create a new one, 'q' to quit)"
     task <- parseTask subDirs <$> getLine
     maybe (readTaskFromInput subDirs) pure task
 
 -- Parses a string into a task. If a task can be parsed, it is returned, otherwise Nothing is returned.
-parseTask :: [ (Int, FP.FilePath) ] -> String -> Maybe Task
+parseTask :: [(Int, FP.FilePath)] -> String -> Maybe Task
 parseTask subDirs "q" = Just Quit
 parseTask subDirs "new" = Just NewProfile
 parseTask subDirs other = number >>= (\n -> find (fst >>> (== n)) subDirs) <&> (snd >>> baseName >>> StartProfile)
-    where number = readMaybe other :: Maybe Int
+  where
+    number = readMaybe other :: Maybe Int
 
 -- Determines the base path for all vscode profiles. This is the subdirectory .code-profiles in the user's home
 -- directory.
@@ -64,7 +63,8 @@ getBasePath = getHomeDirectory <&> flip FP.append (fromText ".code-profiles")
 ensureBasePathExists :: FP.FilePath -> IO ()
 ensureBasePathExists basePath = do
     isDir <- isDirectory basePath
-    if not isDir then do
-        putStrLn "Creating base path"
-        createTree basePath
-    else pure ()
+    if not isDir
+        then do
+            putStrLn "Creating base path"
+            createTree basePath
+        else pure ()
