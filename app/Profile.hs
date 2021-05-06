1d63b41385591bbb10918330e571718ddf0328a2
ThoNohT <e.c.p.bataille@gmail.com>
2021-05-06 21:24:05 +0200

    Implement running a profile

--- app/Profile.hs
+++ app/Profile.hs
@@ -7,8 +7,9 @@ import Data.Text (Text)
 import qualified Data.Text as T (isPrefixOf, pack)
 import Environment (baseName)
 import Filesystem (createTree, isDirectory, listDirectory)
-import qualified Filesystem.Path.CurrentOS as FP (FilePath)
+import qualified Filesystem.Path.CurrentOS as FP (FilePath, append, encodeString, fromText)
 import NewProfile (NewProfile (..))
+import qualified System.Process as Proc
 import Text.Printf (printf)
 
 -- Public
@@ -29,7 +30,15 @@ create (MkNewTemplate path) = do
 
 -- Starts a profile.
 start :: Profile -> IO ()
-start profile@(MkProfile p) = putStrLn $ printf "Starting profile %s" $ name profile
+start profile@(MkProfile path) =
+    let extsPath = FP.encodeString $ FP.append path (FP.fromText "exts")
+        dataPath = FP.encodeString $ FP.append path (FP.fromText "data")
+        command = printf "code --extensions-dir %s --user-data-dir %s ." extsPath dataPath
+     in do
+            putStrLn $ printf "Starting profile %s" $ name profile
+            putStrLn command
+            _ <- Proc.createProcess $ Proc.shell $ command
+            pure ()
 start (MkTemplate p) = putStrLn "Templates are not yet supported."
 
 -- Lists all profiles in a path.
